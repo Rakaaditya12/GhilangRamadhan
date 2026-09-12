38ae3b8788478ee64d5a7c05fc2869a0cffe79a9
Raka Aditya <raka71088@gmail.com>
2026-09-13 00:40:58 +0700

    Update status

--- project.js
+++ project.js
@@ -27,11 +27,9 @@ window.addEventListener("scroll", (e) => {
   e.preventDefault();
 
   if (window.scrollY < 1500) {
-    contentAbout.style.transform = "translateY(3000px)";
     contentAbout.style.transition = "0.8s";
     contentAbout.style.opacity = "0";
   } else if (window.scrollY > 1500) {
-    contentAbout.style.transform = "";
     contentAbout.style.opacity = "1";
   }
 });
