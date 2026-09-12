bab4e2ef48338d78401acd97a027752e3d7f44e1
Raka Aditya <raka71088@gmail.com>
2026-09-13 02:57:01 +0700

    Update status

--- project.js
+++ project.js
@@ -12,10 +12,10 @@ const aktivitas = document.getElementById("content-aktivitas");
 const logoPuncakSaya = document.getElementById("logo-puncaksaya");
 const queryPerjalanan = document.getElementById("query-perjalanan");
 const button = document.getElementById("btn-click");
-const menu = document.getElementById("menu");
 
 button.addEventListener("click", () => {
   setTimeout(() => {
+    const menu = document.getElementById("menu");
     menu.classList.toggle("active");
   }, 400);
 });
