d0eb0c8d99056e916c4bc62207f5b8dc962fa779
Raka Aditya <raka71088@gmail.com>
2026-09-14 16:31:38 +0700

    Update status

--- project.js
+++ project.js
@@ -11,7 +11,6 @@ const navbarContact = document.getElementById("navbar-contact");
 const aktivitas = document.getElementById("content-aktivitas");
 const logoPuncakSaya = document.getElementById("logo-puncaksaya");
 const queryPerjalanan = document.getElementById("query-perjalanan");
-// const button = document.getElementById("btn-click");
 
 window.addEventListener("scroll", (e) => {
   e.preventDefault();
